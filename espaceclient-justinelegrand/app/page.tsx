f787ed1bfb2c590c4235b54653d9847012fa531a
Justine Legrand <jlegrand.osteopatheanimalier@orange.fr>
2025-12-09 19:16:05 +0100

    fix callback 3

--- espaceclient-justinelegrand/app/page.tsx
+++ espaceclient-justinelegrand/app/page.tsx
@@ -1,8 +1,7 @@
+import Loading from 'components/loading';
+
 export default function Home() {
-    return (
-        <div style={{ padding: 50, textAlign: 'center' }}>
-            <h1>Chargement de l'application...</h1>
-            <p>Si vous voyez ceci, le Middleware n'a pas redirigé.</p>
-        </div>
-    )
+    // On affiche simplement ton composant de chargement existant
+    // en attendant que le Middleware redirige l'utilisateur
+    return <Loading />;
 }
