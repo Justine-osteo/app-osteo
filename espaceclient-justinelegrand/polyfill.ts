196483f9782522bcf3c1f9515883d5ec2d9f15c5
Justine Legrand <jlegrand.osteopatheanimalier@orange.fr>
2025-12-09 14:50:30 +0100

    fix erreur FINAL 14

--- espaceclient-justinelegrand/polyfill.ts
+++
@@ -1,5 +0,0 @@
-// Ce fichier définit la variable __dirname qui manque sur Vercel.
-// On force une valeur vide pour que la librairie Supabase ne plante pas.
-if (typeof globalThis.__dirname === 'undefined') {
-    (globalThis as any).__dirname = '';
-}
