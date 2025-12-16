0a1f323fb08c670e348dd234ead79dcf2166a2ea
Justine Legrand <jlegrand.osteopatheanimalier@orange.fr>
2025-12-16 21:38:13 +0100

    changement adresse

--- espaceclient-justinelegrand/app/page.tsx
+++ espaceclient-justinelegrand/app/page.tsx
@@ -1,7 +1,19 @@
-import Loading from 'components/loading';
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+// On utilise ton joli composant de chargement qu'on a utilisé ailleurs
+import EcranDeChargement from '@/components/ui/EcranDeChargement'
 
 export default function Home() {
-    // On affiche simplement ton composant de chargement existant
-    // en attendant que le Middleware redirige l'utilisateur
-    return <Loading />;
+    const router = useRouter()
+
+    useEffect(() => {
+        // Redirection automatique dès que la page se charge
+        // On envoie vers '/mon-espace'. 
+        // Si l'utilisateur n'est pas connecté, la protection de '/mon-espace' le renverra vers '/connexion'
+        router.replace('/mon-espace')
+    }, [router])
+
+    return <EcranDeChargement texte="Lancement de l'application..." />
 }
